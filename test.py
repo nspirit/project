2a6be7c8eb3b0f4c83412a30723c6adcdac3d404
nspirit <nspirit@gmail.com>
2021-03-09 20:53:58 +0300

    healthiest ready

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import random
 
 humans = ['knite', 'bishop', 'landlord']
 humansOpts = {'knite': [100, 0.5], 'bishop': [80, 0.9], 'landlord': [120, 0.3]}
-monsters = ['dragon', 'zombie', 'warewolf']
-monstersOpts = {'dragon' : [200, 0.1], 'zombie' : [150, 0.02], 'warewolf' : [100, 0.5]}
+monsters = ['dragon', 'zombie', 'warlock']
+monstersOpts = {'dragon' : [200, 0.8], 'zombie' : [150, 0.02], 'warewolf' : [100, 0.5]}
 lastTurn = []
 
 def checkArmieStatus(humans, monsters):
@@ -25,17 +25,12 @@ def turn(army, abilities):
     def chooseTheHealthiest(abilities):
         picked = {} 
         for key in abilities:
-            print(key)
-            print(abilities[key][0])
-            picked = {key : abilities[key][0]}
-            # if picked:
-            #     if picked[0][0] > {key, abilities[key][0]}:
-            #         pass
-            #     else:
-            #         picked = {key : abilities[key]}
-            # else:
-            #     picked = {key : abilities[key]}
-        print(picked) 
+            picked[abilities[key][0]] = key
+        chosen = sorted(picked).pop(-1)
+        for key in picked:
+            if key == chosen:
+                ourHero = [picked[key], key]
+        print(ourHero)
     chooseTheHealthiest(abilities)
 
 def event_loop():
